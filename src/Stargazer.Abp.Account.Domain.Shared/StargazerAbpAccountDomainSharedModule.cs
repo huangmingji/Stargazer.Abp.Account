66b198e4ce6b0d1f8a07a6709150639548d17b18
huangmingji <huangmingji@stargazer.tech>
2024-05-23 23:49:43 +0800

    修复本地化无效的问题

--- src/Stargazer.Abp.Account.Domain.Shared/StargazerAbpAccountDomainSharedModule.cs
+++ src/Stargazer.Abp.Account.Domain.Shared/StargazerAbpAccountDomainSharedModule.cs
@@ -24,13 +24,6 @@ namespace Stargazer.Abp.Account.Domain.Shared
                 options.Resources
                     .Add<AccountResource>("zh-Hans")
                     .AddVirtualJson("/Localization/Resources/Account");
-
-                options.Languages.Add(new LanguageInfo("en", "en", "English"));
-                options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-                foreach (var key in options.Resources.Keys)
-                {
-                    options.Resources[key].DefaultCultureName = "zh-Hans";
-                }
             });
         }
     }
