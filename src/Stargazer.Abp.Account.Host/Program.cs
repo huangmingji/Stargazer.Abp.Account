6448eb0118ed99ce19db6801052d43182cf4e896
huangmingji <huangmingji@stargazer.tech>
2024-12-30 14:58:16 +0800

    升级类库至Net9.0

--- src/Stargazer.Abp.Account.Host/Program.cs
+++ src/Stargazer.Abp.Account.Host/Program.cs
@@ -1,50 +1,45 @@
-using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Scalar.AspNetCore;
+using Microsoft.AspNetCore.OpenApi;
 using Serilog;
-using Serilog.Events;
+using Stargazer.Abp.Account.Host;
 
-namespace Stargazer.Abp.Account.Host
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseAutofac().UseSerilog();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.Async(c =>
+        c.File("Logs/log.txt",
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 31,
+            rollOnFileSizeLimit: true,
+            fileSizeLimitBytes: 31457280,
+            buffered: true))
+    .WriteTo.Console()
+    .CreateLogger();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+try
 {
-    public class Program
+    // Add services to the container.
+    builder.Services.ReplaceConfiguration(builder.Configuration);
+    builder.Services.AddApplication<StargazerAbpAccountHostModule>();
+    builder.Services.AddOpenApi();
+    var app = builder.Build();
+    app.InitializeApplication();
+    if (!app.Environment.IsProduction())
     {
-        public static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Async(c =>
-                    c.File("Logs/log.txt",
-                        rollingInterval: RollingInterval.Day,
-                        retainedFileCountLimit: 31,
-                        rollOnFileSizeLimit: true,
-                        fileSizeLimitBytes: 31457280,
-                        buffered: true))
-                .WriteTo.Console()
-                .CreateLogger();
-            try
-            {
-                Log.Information("Starting web host");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Host terminated unexpectedly");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .UseAutofac()
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.MapScalarApiReference();
+        app.MapOpenApi();
     }
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
